1dfc39a3424b69f1fa1827888d288069714d472f
sacheth <sacheth@zktecodev.com>
2024-12-03 15:11:29 +0530

    Refactor

--- Sources/DemoProject/DemoProject.swift
+++ Sources/DemoProject/DemoProject.swift
@@ -4,17 +4,12 @@
 import Foundation
 import UIKit
 
-//public extension UIViewController{
-//    func navigateToPackage(){
-//        let sb = UIStoryboard(name: "DemoStoryBoard", bundle: Bundle.main)
-//        let vc = sb.instantiateViewController(withIdentifier: "FirstVC")
-//        self.navigationController?.pushViewController(vc, animated: true)
-//    }
-//}
-
-public class NavigateUI{
-    @MainActor static var main = UIWindow()
-    @MainActor static  func navigateToPackage(){
-        main = UIApplication.shared.windows.first!
+public extension UIViewController{
+    func navigateToPackage(){
+        let sb = UIStoryboard(name: "DemoStoryBoard", bundle: Bundle.module)
+        let vc = sb.instantiateViewController(withIdentifier: "FirstVC")
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
+
+
