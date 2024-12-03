3cfd0d0de6c6058e7cd444af8022b35edfee6ff5
sacheth <sacheth@zktecodev.com>
2024-12-03 14:14:47 +0530

    Resolved issue.

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DemoProject"),
-        .testTarget(
-            name: "DemoProjectTests",
-            dependencies: ["DemoProject"]
-        ),
+            name: "DemoProject",
+            resources: [
+                .process("Resources") // Add this to process resources
+            ])
+        ,
     ]
 )
--- Sources/DemoProject/DemoProject.swift
+++ Sources/DemoProject/DemoProject.swift
@@ -1,11 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
 import Foundation
 import UIKit
 
 public extension UIViewController{
     func navigateToPackage(){
-        let sb = UIStoryboard(name: "DemoStoryBoard", bundle: nil)
+        let sb = UIStoryboard(name: "DemoStoryBoard", bundle: Bundle.module)
         let vc = sb.instantiateViewController(withIdentifier: "FirstVC")
         self.navigationController?.pushViewController(vc, animated: true)
     }
