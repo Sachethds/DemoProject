1f0ad51ff3a1aca7058986a5cbe72cb47a617bb8
sacheth <sacheth@zktecodev.com>
2024-12-03 14:45:05 +0530

    update

--- Sources/DemoProject/DemoProject.swift
+++ Sources/DemoProject/DemoProject.swift
@@ -6,7 +6,7 @@ import UIKit
 
 public extension UIViewController{
     func navigateToPackage(){
-        let sb = UIStoryboard(name: "DemoStoryBoard", bundle: Bundle.module)
+        let sb = UIStoryboard(name: "DemoStoryBoard", bundle: Bundle.main)
         let vc = sb.instantiateViewController(withIdentifier: "FirstVC")
         self.navigationController?.pushViewController(vc, animated: true)
     }
